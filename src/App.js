d98fc1b41c7c6082b913ee51765fcb0c37762598
ackarlse <ann.c.karlsen@hiof.no>
2023-02-14 14:42:52 +0100

    Opprettet shopping branch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import { useState } from 'react';
 import './App.css'
 import ContentPage from './components/ContentPage';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import './css/main.css'
 
+const cartStart = [
+  {
+    id: 0,
+    title: "",
+    price: 0, 
+
+  }
+]
 
 function App() {
+  const [cartItems, setCartItem] = useState([...cartStart])
+  
   return (
     <div className="container">
         <Header/>
         <Nav />
-        <ContentPage/>
+        <ContentPage cartItems={cartItems} setCartItem={setCartItem}/>
     </div>
   );
 }
